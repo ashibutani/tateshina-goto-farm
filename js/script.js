8a03b8cf8282b5a8d5e9b4a075334a7056b4740e
akiko <shibutani0726@gmail.com>
2023-05-23 22:39:18 +0900

    product swiper

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
-// $(function(){
-//     $(".hm_menu").on("click",function(){
-//         $(".sp_nav").slideToggle(300);
-//         $(".hm_open, .hm_close").toggleClass("active");       
 
-//     });
-// });
-
-
-// $(function () {
-//     $(".sp_nav ul li a").on("click", function () {
-
-//         $(".sp_nav").fadeOut();
-//         $(".hm_open, .hm_close").toggleClass("active"); 
-//     });
-// });
 
 $(function () {
     $(".hm_menu").on("click", function () {
@@ -30,3 +15,22 @@ $(function () {
         $(".hm_menu span").removeClass("active");/*クラスactiveをつけるとボタンがななめになるので、クラスactiveを消す*/
     });
 });
+
+
+
+const swiper = new Swiper(".swiper", {
+
+    slidesPerView: 'auto', // コンテナ内に表示させるスライド数（CSSでサイズ指定する場合は 'auto'）
+    spaceBetween: 24, // スライド間の余白（px）
+    centeredSlides: true, // アクティブなスライドを中央に配置する
+
+
+    loop: true,  // 無限ループさせる
+    loopedSlides: 8,
+
+
+    speed: 300, // スライドアニメーションのスピード（ミリ秒）
+
+
+});
+
